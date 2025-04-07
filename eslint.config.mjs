c58afd279510154f4042f8e745320b67e2af7f00
Ruslan Mukhamedvaleev <r1554899@gmail.com>
2025-04-07 03:45:02 -0700

    Configure ESLint rules to disable various checks for improved flexibility in development.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,37 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "@next/next/no-html-link-for-pages": "off",
+      "@next/next/no-img-element": "off",
+      "@next/next/no-sync-scripts": "off",
+      "@typescript-eslint/ban-ts-comment": "off",
+      "@typescript-eslint/ban-types": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-unused-vars": "off",
+      "@typescript-eslint/no-var-requires": "off",
+      "import/no-anonymous-default-export": "off",
+      "jsx-a11y/alt-text": "off",
+      "jsx-a11y/aria-props": "off",
+      "jsx-a11y/aria-proptypes": "off",
+      "jsx-a11y/aria-unsupported-elements": "off",
+      "jsx-a11y/role-has-required-aria-props": "off",
+      "jsx-a11y/role-supports-aria-props": "off",
+      "react-hooks/exhaustive-deps": "off",
+      "react-hooks/rules-of-hooks": "off",
+      "react/display-name": "off",
+      "react/jsx-key": "off",
+      "react/jsx-no-target-blank": "off",
+      "react/no-unescaped-entities": "off",
+      "react/prop-types": "off",
+      "no-restricted-syntax": "off",
+      "no-unused-vars": "off",
+      "no-undef": "off",
+      "no-console": "off",
+      "**": "off",
+    },
+  },
 ];
 
 export default eslintConfig;
